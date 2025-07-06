23a76399c368b8b2ccce43334c24ca3012ccb823
Sambhav Jain <sambhav.jain2002@gmail.com>
2025-07-06 12:48:17 +0530

    `Added new routes and data loading functionality to main.py, updated .gitignore to ignore new files.`

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+import json
 
 app =FastAPI()
+def load_data():
+    with open('patients.json', 'r') as file:
+        data= json.load(file)
+    return data
+
 
 @app.get("/")
 def hello():
-    return {'message':'hello world'}
+    return {'message':'Patient Management System API'}
+
 
 @app.get("/about")
 def about():
-    return {'message':' this is about page'}  
+    return {'message':'This is the Patient Management System API, designed to manage patient records and appointments.'}
+
+@app.get("/view")
+def view():
+    
+    data = load_data()
+    return data
